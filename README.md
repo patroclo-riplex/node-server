d58a107da98fe4cacebc7220eb480210f06a8f94
patroclo-riplex <gofo_02@hotmail.com>
2023-09-20 11:45:58 -0500

    Lista de tareas asíncrona en node

---
+++ README.md
@@ -0,0 +1,6 @@
+¿Qué sucedio al usar async y await?
+async y await hacen que el código asíncrono se vea y se comporte de manera similar al código síncrono, facilitando la legibilidad y mantenimiento del código. Al usar await, la ejecución espera a que la promesa se resuelva antes de pasar a la siguiente instrucción. Esto permite un estilo de programación más sincrónico en una función asíncrona.
+¿Qué sucedio al usar el método then()?
+Al usar el método then(), estamos manejando la promesa usando una estructura de estilo de callback. La función dentro de then() se ejecuta cuando la promesa se resuelve correctamente, en otras palabras, usaremos then cuando se quiera manejar el resultado de una promesa o encadenar operaciones adicionales después de que la promesa se resuelva.
+¿Qué diferencias encontraste entre async, await y el método then()?
+async/await es más legible y se asemeja a la programación síncrona, mientras que then() sigue un estilo de programación basado en callbacks. async/await es más conveniente y legible para trabajar con operaciones asincrónicas, especialmente cuando se necesita encadenar múltiples operaciones. then() es útil y ampliamente utilizado, pero puede llevar a un estilo de código más anidado.La elección entre ellos dependerá de la preferencia del usuario y de la estructura y complejidad del código. 
--- tareas.js
+++ tareas.js
@@ -3,42 +3,53 @@ const readline = require('readline-sync');
 const tareas = [];
 
 function agregarTarea() {
-  const indicador = tareas.length + 1;
-  const descripcion = readline.question('Introduce la descripcion de la tarea: ');
-  const estado = 'No completada';
+  return new Promise((resolve, reject) => {
+    const indicador = tareas.length + 1;
+    const descripcion = readline.question('Introduce la descripcion de la tarea: ');
+    const estado = 'No completada';
 
-  tareas.push({ indicador, descripcion, estado });
-  console.log('Tarea añadida correctamente.');
+    tareas.push({ indicador, descripcion, estado });
+    console.log('Tarea añadida correctamente.');
+    resolve();
+  });
 }
 
 function eliminarTarea() {
-  console.log('Lista de tareas:');
-  mostrarTareas();
+  return new Promise((resolve, reject) => {
+    console.log('Lista de tareas:');
+    mostrarTareas();
 
-  const tareaAEliminar = parseInt(readline.question('Introduce el indicador de la tarea a eliminar: '));
+    const tareaAEliminar = parseInt(readline.question('Introduce el indicador de la tarea a eliminar: '));
 
-  const index = tareas.findIndex(tarea => tarea.indicador === tareaAEliminar);
-  if (index !== -1) {
-    tareas.splice(index, 1);
-    console.log('Tarea eliminada correctamente.');
-  } else {
-    console.log('Indicador de tarea no valido.');
-  }
+    const index = tareas.findIndex(tarea => tarea.indicador === tareaAEliminar);
+    if (index !== -1) {
+      tareas.splice(index, 1);
+      console.log('Tarea eliminada correctamente.');
+      resolve();
+    } else {
+      console.log('Indicador de tarea no valido.');
+      reject();
+    }
+  });
 }
 
 function completarTarea() {
-  console.log('Lista de tareas:');
-  mostrarTareas();
+  return new Promise((resolve, reject) => {
+    console.log('Lista de tareas:');
+    mostrarTareas();
 
-  const tareaACompletar = parseInt(readline.question('Introduce el indicador de la tarea a completar: '));
+    const tareaACompletar = parseInt(readline.question('Introduce el indicador de la tarea a completar: '));
 
-  const tarea = tareas.find(tarea => tarea.indicador === tareaACompletar);
-  if (tarea) {
-    tarea.estado = 'Completada';
-    console.log('Tarea marcada como completada.');
-  } else {
-    console.log('Indicador de tarea no valido.');
-  }
+    const tarea = tareas.find(tarea => tarea.indicador === tareaACompletar);
+    if (tarea) {
+      tarea.estado = 'Completada';
+      console.log('Tarea marcada como completada.');
+      resolve();
+    } else {
+      console.log('Indicador de tarea no válido.');
+      reject();
+    }
+  });
 }
 
 function mostrarTareas() {
@@ -47,32 +58,93 @@ function mostrarTareas() {
   });
 }
 
-while (true) {
-  console.log('\nOpciones:');
-  console.log('1. Agregar tarea');
-  console.log('2. Eliminar tarea');
-  console.log('3. Completar tarea');
-  console.log('4. Mostrar tareas');
-  console.log('5. Salir');
+async function main() {
+  while (true) {
+    console.log('\nOpciones:');
+    console.log('1. Agregar tarea');
+    console.log('2. Eliminar tarea');
+    console.log('3. Completar tarea');
+    console.log('4. Mostrar tareas');
+    console.log('5. Salir');
 
-  const opcion = parseInt(readline.question('Elige una opcion: '));
+    const opcion = parseInt(readline.question('Elige una opcion: '));
 
-  switch (opcion) {
-    case 1:
-      agregarTarea();
-      break;
-    case 2:
-      eliminarTarea();
-      break;
-    case 3:
-      completarTarea();
-      break;
-    case 4:
-      mostrarTareas();
-      break;
-    case 5:
-      process.exit(0);
-    default:
-      console.log('Opcion no valida. Intentalo de nuevo.');
+    try {
+      switch (opcion) {
+        case 1:
+          await agregarTarea();
+          break;
+        case 2:
+          await eliminarTarea();
+          break;
+        case 3:
+          await completarTarea();
+          break;
+        case 4:
+          mostrarTareas();
+          break;
+        case 5:
+          process.exit(0);
+        default:
+          console.log('Opción no valida. Intentalo de nuevo.');
+      }
+    } catch (error) {
+      console.error('Ocurrio un error:', error);
+    }
   }
 }
+
+main();
+
+
+//Probando implementando then
+
+// async function main() {
+//   while (true) {
+//     console.log('\nOpciones:');
+//     console.log('1. Agregar tarea');
+//     console.log('2. Eliminar tarea');
+//     console.log('3. Completar tarea');
+//     console.log('4. Mostrar tareas');
+//     console.log('5. Salir');
+
+//     const opcion = parseInt(readline.question('Elige una opción: '));
+
+//     try {
+//       switch (opcion) {
+//         case 1:
+//           agregarTarea().then(() => {
+//             console.log('Operación de agregar tarea completada.');
+//           }).catch(error => {
+//             console.error('Ocurrió un error:', error);
+//           });
+//           break;
+//         case 2:
+//           eliminarTarea().then(() => {
+//             console.log('Operación de eliminar tarea completada.');
+//           }).catch(error => {
+//             console.error('Ocurrió un error:', error);
+//           });
+//           break;
+//         case 3:
+//           completarTarea().then(() => {
+//             console.log('Operación de completar tarea completada.');
+//           }).catch(error => {
+//             console.error('Ocurrió un error:', error);
+//           });
+//           break;
+//         case 4:
+//           mostrarTareas();
+//           break;
+//         case 5:
+//           process.exit(0);
+//         default:
+//           console.log('Opción no válida. Inténtalo de nuevo.');
+//       }
+//     } catch (error) {
+//       console.error('Ocurrió un error:', error);
+//     }
+//   }
+// }
+
+// main();
